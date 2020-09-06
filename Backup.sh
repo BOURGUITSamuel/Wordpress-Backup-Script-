55b0a7c2dbb2cc858438883654a938fea63328aa
Jean - Samuel Eric BOURGUIT <65498199+KaizerShibbyWan@users.noreply.github.com>
2020-09-06 15:36:24 +0200

    Update Backup.sh
    final version

--- Backup.sh
+++ Backup.sh
@@ -3,18 +3,21 @@
 #### Settings ####
 NOW=$(date +"%Y-%m-%d-%H%M")
 BACKUP_FOLDER=/backup/
-RETENTION=7
 
 #### Site-specific Info ####
 SITE_PATH="/var/www/html/wordpress/" #Could also be subsites/subsitename
+SITE_CONF="/etc/apache2/sites-available/" #virtual_host apache config
 DB_NAME=`cat /var/www/html/wordpress/wp-config.php | grep DB_NAME | cut -d \' -f 4`
 DB_USER=`cat /var/www/html/wordpress/wp-config.php | grep DB_USER | cut -d \' -f 4`
 DB_PASS=`cat /var/www/html/wordpress/wp-config.php | grep DB_PASSWORD | cut -d \' -f 4`
 DB_HOST=`cat /var/www/html/wordpress/wp-config.php | grep DB_HOST | cut -d \' -f 4`
 
+#### Retention ####
+RETENTION=7
+
 #### Files backup ####
 function files_backup {
-sudo tar -cvf /backup/$NOW.tar.gz $SITE_PATH $SITE_CONF 
+sudo tar -cvf $BACKUP_FOLDER$NOW.tar.gz $SITE_PATH $SITE_CONF 
 }
 
 #### Database Backup ####
@@ -25,8 +28,8 @@ sudo mv $DB_NAME.$NOW.sql $BACKUP_FOLDER/$DB_NAME.$NOW.sql
 
 #### Scp Transfert ####
 function scp_transfert {
-sudo scp $BACKUP_FOLDER/*.tar.gz root@ftpserver:/backup/
-sudo scp $BACKUP_FOLDER/*.sql root@ftpserver:/backup/
+sudo scp $BACKUP_FOLDER$NOW.tar.gz root@ftpserver:/backup/
+sudo scp $BACKUP_FOLDER$DB_NAME.$NOW.sql root@ftpserver:/backup/
 }
 
 #### Delete old data with retention option ####
